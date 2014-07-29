db0e348e61862db75181c9d432719ebd287b56e7
kevwargo <jarasz.kevwargo@gmail.com>
2014-07-29 13:05:57 +0300

    History in ielm without cycling (new functions comint-previous-input-straight and comint-next-input-straight)

--- config/comint.el
+++ config/comint.el
@@ -1,16 +1,42 @@
+(defun comint-previous-input-straight (arg)
+  "Move backwards in input history (without cycling)"
+  (interactive "*p")
+  (cond
+   ((and comint-input-ring-index
+         (< arg 0)
+         (eq comint-input-ring-index 0)
+         comint-stored-incomplete-input)
+    (comint-restore-input))
+   ((or (and (< arg 0)
+             (null comint-input-ring-index))
+        (and (> arg 0)
+             (eq comint-input-ring-index
+                 (1- (ring-length comint-input-ring)))))
+    nil)
+   (t
+    (comint-previous-matching-input "." arg))))
+
+(defun comint-next-input-straight (arg)
+  "Move forward in input history (without cycling)"
+  (interactive "*p")
+  (comint-previous-input-straight (- arg)))
+
+
 (defun comint-up-or-prev-input (&optional arg)
   (interactive "*^p")
   (if (comint-after-pmark-p)
-      (comint-previous-input arg)
+      (comint-previous-input-straight arg)
     (previous-line arg)))
 
 (defun comint-down-or-next-input (&optional arg)
   (interactive "*^p")
   (if (comint-after-pmark-p)
-      (comint-next-input arg)
+      (comint-next-input-straight arg)
     (next-line arg)))
 
 (defun comint-mode-keymap-modify ()
+  ;; (local-set-key [up] 'comint-previous-input)
+  ;; (local-set-key [down] 'comint-next-input)
   (local-set-key [up] 'comint-up-or-prev-input)
   (local-set-key [down] 'comint-down-or-next-input)
   (local-set-key (kbd "C-<up>") 'previous-line)
