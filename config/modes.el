f319b9a1c0a57a449cb89c22f1c8710079f03fe8
kevwargo <jarasz.kevwargo@gmail.com>
2015-02-05 23:33:41 +0100

    adding php-mode

--- config/modes.el
+++ config/modes.el
@@ -6,6 +6,7 @@
 
 (add-to-list 'load-path (concat kec:config-dir "undo-tree"))
 (add-to-list 'load-path (concat kec:config-dir "rules-editing-mode"))
+(add-to-list 'load-path (concat kec:config-dir "php-mode-1.13.1"))
 
 (load "pddl-mode")
 (load "my-drools")
@@ -37,5 +38,9 @@
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 (electric-pair-mode 1)
+
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
 (add-hook 'after-change-major-mode-hook 'enable-linum-in-some-buffers)
