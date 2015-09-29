bc4e2c41fe8d1de270dda3a707cc6f073db16f8d
kevwargo <jarasz.kevwargo@gmail.com>
2015-09-29 09:46:28 +0300

    add apostrophe to electric-pair-pairs in web-mode, plus some extension fixes

--- config/web.el
+++ config/web.el
@@ -1,6 +1,6 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
@@ -12,11 +12,13 @@
 
 (defun web-mode-keymap-modify ()
   (local-unset-key (kbd "M-;"))
+  (local-set-key (kbd "C-{") 'c-embrace-selected-lines)
   (local-set-key (kbd "C-d") 'web-mode-comment-or-uncomment)
   (local-set-key (kbd "C-S-d") 'web-mode-comment-or-uncomment))
 
 (defun web-mode-hook-misc ()
   (electric-indent-mode 0)
+  (setq electric-pair-pairs (append electric-pair-pairs (list '(?' . ?'))))
   (setq indent-tabs-mode t))
 
 (add-hook 'web-mode-hook 'web-mode-keymap-modify)
