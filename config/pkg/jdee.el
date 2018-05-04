1c7b10b4a1f4205f07b5cd7db5433baa7155c444
kevwargo <jarasz.kevwargo@gmail.com>
2018-05-04 12:01:40 +0200

    slightly better ac-jdee-candidates

--- config/pkg/jdee.el
+++ config/pkg/jdee.el
@@ -1,41 +1,49 @@
 (require 'jdee)
 (require 'auto-complete)
 
+(defun ac-jdee--build-method-snippet (method)
+  (let* ((method (butlast method))
+         (name (car method))
+         (return (cadr method))
+         (args (mapcar (lambda (class)
+                         (string-match "\\.?[[:space:]]*\\([a-zA-Z_][0-9a-zA-Z_]*\\)\\'" class)
+                         (match-string 1 class))
+                       (cddr method))))
+    (cons
+     (concat name "(" (mapconcat 'identity args ", ") ")")
+     (concat name "("
+             (mapconcat
+              'identity
+              (let ((index 1)
+                    (arg args)
+                    list)
+                (while (car arg)
+                  (setq list (append list (list (format "${%d:%s}"
+                                                        index
+                                                        (car arg)))))
+                  (setq index (1+ index))
+                  (setq arg (cdr arg)))
+                list)
+              ", ")
+             ")$0"))))
+
 (defun ac-jdee-candidates ()
-  (ignore-errors
-    (let* ((pair (jdee-parse-java-variable-at-point))
-           (type (flet ((y-or-n-p (format) nil)) ; pretty dirty hack
-                   (jdee-parse-eval-type-of (car pair))))
-           (classinfo (car (jdee-complete-invoke-get-class-info type jdee-complete-public)))
-           (fields (nth jdee-complete-fields classinfo))
-           (methods (nth jdee-complete-methods classinfo)))
-      (append
-       (mapcar (lambda (method)
-                 (let* ((method (butlast method))
-                        (name (car method))
-                        (return (cadr method))
-                        (args (mapcar (lambda (class)
-                                        (string-match "\\.?\\([a-zA-Z_][0-9a-zA-Z_]*\\)\\'" class)
-                                        (match-string 1 class))
-                                      (cddr method))))
-                   (cons
-                    (concat name "(" (mapconcat 'identity args ", ") ")")
-                    (concat name "("
-                            (mapconcat
-                             'identity
-                             (let ((index 1)
-                                   (arg args)
-                                   list)
-                               (while (car arg)
-                                 (setq list (append list (list (format "${%d:%s}"
-                                                                       index
-                                                                       (car arg)))))
-                                 (setq index (1+ index))
-                                 (setq arg (cdr arg)))
-                               list)
-                             ", ")
-                            ")$0"))))
-               methods)))))
+  (condition-case err
+      (let* ((pair (let ((p (jdee-parse-java-variable-at-point)))
+                     (destructuring-bind (c . f) p
+                       (if (= (aref c (1- (length c))) ?.)
+                           (cons (substring c 0 (1- (length c)))
+                                 (cdr p))
+                         p))))
+             (type (flet ((y-or-n-p (format) nil)) ; pretty dirty hack
+                     (jdee-parse-eval-type-of (car pair))))
+             (classinfo (car (jdee-complete-invoke-get-class-info type jdee-complete-public)))
+             (fields (nth jdee-complete-fields classinfo))
+             (methods (mapcar 'ac-jdee--build-method-snippet
+                              (nth jdee-complete-methods classinfo))))
+        methods)
+    (error
+     (message "Error during jdee auto-complete: %S" err))))
 
 (defun ac-jdee-action ()
   (let* ((last-complete-string (cdr ac-last-completion))
@@ -46,7 +54,7 @@
 
 (ac-define-source jdee
   '((candidates . ac-jdee-candidates)
-    (prefix . "\\.[\\n\\r\\t ]*\\([a-zA-Z_]?[0-9a-zA-Z_]*\\)")
+    (prefix . "\\.[[:space:]]*\\([a-zA-Z_]?[0-9a-zA-Z_]*\\)")
     (requires . 0)
     (action . ac-jdee-action)))
 
