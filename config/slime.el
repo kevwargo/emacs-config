24334944c01bb58e5602139ae77dc6ff900e40cd
jarasz.lojka <jarasz.lojka@mindseater.com>
2018-05-10 12:25:38 +0200

    slime-stumpwm

--- config/slime.el
+++ config/slime.el
@@ -12,4 +12,13 @@
 (defun slime-repl-custom-keys-hook ()
   (local-set-key (kbd "C-c C-k") 'slime-repl-delete-current-input))
 
+(defun slime-set-package-to-stumpwm ()
+  (if (= (slime-connection-port (slime-current-connection)) 44005)
+      (slime-repl-set-package ":stumpwm")))
+
+(defun slime-stumpwm ()
+  (interactive)
+  (slime-connect "localhost" 44005))
+
 (add-hook 'slime-repl-mode-hook 'slime-repl-custom-keys-hook)
+(add-hook 'slime-repl-mode-hook 'slime-set-package-to-stumpwm)
