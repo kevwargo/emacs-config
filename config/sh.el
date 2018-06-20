ccf06d01eae3c8b101ec10528d25124c2f0ddcd8
jarasz.lojka <jarasz.lojka@mindseater.com>
2018-06-20 14:34:30 +0200

    Unset C-c C-z in sh-mode-keymap

--- config/sh.el
+++ config/sh.el
@@ -1,5 +1,6 @@
 (defun sh-mode-customize ()
   (local-unset-key (kbd "C-c C-\\"))
+  (local-unset-key (kbd "C-c C-z"))
   (local-unset-key (kbd "C-c C-d")))
 
 (add-hook 'sh-mode-hook 'sh-mode-customize)
