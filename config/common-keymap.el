a7e70d4ba8b777f009863ab3cd726cd2a4e9f49b
kevwargo <jarasz.kevwargo@gmail.com>
2018-10-23 13:26:55 +0200

    Return default binding for C-t

--- config/common-keymap.el
+++ config/common-keymap.el
@@ -58,7 +58,6 @@
 (global-set-key (kbd "C-x f <down>") 'find-file-other-window-down)
 (global-set-key (kbd "C-x f b") 'find-file-from-buffer)
 
-(global-set-key (kbd "C-t") 'multi-term)
 (global-set-key (kbd "C-c t") (make-sparse-keymap))
 (global-set-key (kbd "C-c t <up>") 'multi-term-up)
 (global-set-key (kbd "C-c t <down>") 'multi-term-down)
