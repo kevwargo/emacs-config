8241336a925d522597c4fd954b6c1d2caf863934
kevwargo <jarasz.kevwargo@gmail.com>
2018-10-23 14:14:22 +0200

    jedi fixes

--- config/pkg/jedi.el
+++ config/pkg/jedi.el
@@ -1,3 +1,26 @@
-(add-hook 'python-mode-hook 'jedi:setup)
+(require 's)
+(require 'jedi-core)
+(require 'uuidgen)
+
+(defun jedi-setup ()
+  (let ((venv-path
+         (with-temp-buffer
+           (if (= (call-process shell-file-name
+                                nil
+                                (list t nil)
+                                nil
+                                "-c" "pipenv --venv")
+                  0)
+               (s-trim (buffer-substring-no-properties (point-min)
+                                                       (point-max))))))
+        jedi-args)
+    (when (and venv-path (file-directory-p venv-path))
+      (setq-local jedi:server-args (list
+                                    "--virtual-env" venv-path
+                                    "--log" (concat kec:config-dir "jedi-logs/" (uuidgen-4) ".log")
+                                    "--log-level" "DEBUG"))))
+  (jedi:setup))
+
+(add-hook 'python-mode-hook 'jedi-setup)
 
 (setq jedi:complete-on-dot t)
