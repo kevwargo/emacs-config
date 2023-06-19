f49af0849ab3a25a4b5788964662a57a4d265de4
kevwargo <jarasz.kevwargo@gmail.com>
2023-06-19 11:05:17 +0200

    Fix xref in ielm

--- config/ielm.el
+++ config/ielm.el
@@ -1,4 +1,5 @@
 (defun ielm-custom-hook ()
-  (local-set-key (kbd "C-c C-f") 'find-function))
+  (local-set-key (kbd "C-c C-f") 'find-function)
+  (add-hook 'xref-backend-functions 'elisp--xref-backend nil t))
 
 (add-hook 'ielm-mode-hook 'ielm-custom-hook)
