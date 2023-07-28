81f7733397b1bd2f045b1ac2170a3b228b21e207
kevwargo <jarasz.kevwargo@gmail.com>
2023-07-28 14:23:24 +0200

    Removed unused stuff in go-mode: godef, golangci-lint, godoc-at-point, go-goto-map

--- config/pkg/go.el
+++ config/pkg/go.el
@@ -1,28 +1,7 @@
-(require 'cl-seq)
 (require 'go-tag)
 (require 'lsp)
 
 (defvar-local go-test-verbose nil)
-
-(defun godef-smart-jump ()
-  (interactive)
-  (let ((keys (this-command-keys-vector))
-        (process-environment (append
-                              (cl-remove-if
-                               (lambda (e)
-                                 (or (string= e "GOPATH")
-                                     (and (> (length e) 7)
-                                          (string= (substring e 0 7)
-                                                   "GOPATH="))))
-                               process-environment)
-                              (list (concat "GOPATH="
-                                            (or (go-guess-gopath)
-                                                (go-original-gopath)))))))
-    (cond
-      ((equal keys (kbd "M-."))
-       (godef-jump (point)))
-      ((equal keys (kbd "M->"))
-       (godef-jump-other-window (point))))))
 
 (defun go-toggle-test-verbose ()
   (interactive)
@@ -53,34 +32,13 @@
                            "pascalcase"
                            "keep"))))))
 
-(defun golangci-lint (dir)
-  (interactive
-   (list (ido-read-directory-name
-          "Run golangci-lint in: "
-          default-directory)))
-  (let ((default-directory dir))
-    (compilation-start "golangci-lint run --disable-all -E golint ./...")))
 
-(defun golangci-lint-temp (dir)
-  (interactive
-   (list (ido-read-directory-name
-          "Run golangci-lint in: "
-          default-directory)))
-  (let ((default-directory dir))
-    (compilation-start "git diff master | golangci-lint run --out-format colored-line-number --new-from-patch /dev/stdin ./core/... | sort -V")))
-
-
-(defun go-hook ()
-  (local-set-key (kbd "C-c C-f") 'gofmt)
-  (local-set-key (kbd "C-c C-c") 'go-goto-map)
-  (local-set-key (kbd "M-.") 'godef-smart-jump)
-  (local-set-key (kbd "M->") 'godef-smart-jump)
-  (local-set-key (kbd "C-x h") 'godoc-at-point)
+(defun go-setup-keys ()
   (local-set-key (kbd "C-{") 'embrace-selected-lines)
+  (local-set-key (kbd "C-x V") 'go-toggle-test-verbose)
   (local-set-key (kbd "C-x t") 'go-tag-refresh-json)
-  (local-set-key (kbd "C-x T") 'go-tag-add-json)
+  (local-set-key (kbd "C-x T") 'go-tag-add-json))
 
-  (lsp))
-
-(add-hook 'go-mode-hook 'go-hook)
+(add-hook 'go-mode-hook 'go-setup-keys)
+(add-hook 'go-mode-hook 'lsp)
 (add-hook 'before-save-hook 'gofmt-before-save)
