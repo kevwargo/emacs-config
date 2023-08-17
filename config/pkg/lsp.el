b1bb990b016787b9f3f2c978e5aba1e9c60d2a66
kevwargo <jarasz.kevwargo@gmail.com>
2023-08-17 16:56:26 +0200

    Respect setup.py as well as Pipfile as a python project designator

--- config/pkg/lsp.el
+++ config/pkg/lsp.el
@@ -3,24 +3,30 @@
 
 (defvar lsp-modes
   '((go-mode . "go.mod")
-    (python-mode . "Pipfile")
+    (python-mode . ("Pipfile" "setup.py"))
     (typescript-mode . "package.json")
     (java-mode . find-custom-lsp-java-project)))
 
 (defun setup-lsp-mode ()
   (when-let ((file-name (buffer-file-name))
              (project-descriptor (cdr-safe (assoc major-mode lsp-modes))))
-    (let ((project-find-functions (append
-                                   (list
-                                    (lambda (dir)
-                                      (if-let ((prj-dir (locate-dominating-file dir project-descriptor)))
-                                          (progn
-                                            (message "Found project dir for %s (%s)" file-name major-mode)
-                                            (cons 'vc prj-dir))
-                                        (message "Project dir for %s (%s) not found" file-name major-mode)
-                                        nil)))
-                                   project-find-functions)))
-      (lsp))))
+    (cl-labels ((find-prj-dir (dir desc)
+                              (cond ((consp desc)
+                                     (or (locate-dominating-file dir (car desc))
+                                         (find-prj-dir dir (cdr desc))))
+                                    ((stringp desc)
+                                     (find-prj-dir dir (list desc))))))
+      (let ((project-find-functions (append
+                                     (list
+                                      (lambda (dir)
+                                        (if-let ((prj-dir (find-prj-dir dir project-descriptor)))
+                                            (progn
+                                              (message "Found LSP project dir for %s (%s)" file-name major-mode)
+                                              (cons 'vc prj-dir))
+                                          (message "LSP project dir for %s (%s) not found" file-name major-mode)
+                                          nil)))
+                                     project-find-functions)))
+        (lsp)))))
 
 (add-hook 'after-change-major-mode-hook 'setup-lsp-mode)
 (define-key lsp-mode-map (kbd "M-e") #'lsp-execute-code-action)
