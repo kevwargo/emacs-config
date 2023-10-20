4195ea3fe2f091fc6f1d4edda7945fb5faeab675
kevwargo <jarasz.kevwargo@gmail.com>
2023-10-20 18:17:25 +0200

    Bind "C-c ." to find-function in key-overrides-mode

--- utils/key-overrides.el
+++ utils/key-overrides.el
@@ -5,6 +5,7 @@
             (define-key m (kbd "C-x u") 'undo-tree-visualize)
             (define-key m (kbd "C-x SPC") 'mark-current-line)
             (define-key m (kbd "C-x C-n") 'neotree-toggle)
+            (define-key m (kbd "C-c .") 'find-function)
             m)
   :global t)
 
