a08ae622e949d0aaefa7d9f7fbfdcbf78b9e9914
kevwargo <jarasz.kevwargo@gmail.com>
2024-03-08 21:03:10 +0100

    Change flymake-goto-*-error bindings

--- config/flymake.el
+++ config/flymake.el
@@ -1,4 +1,4 @@
 (require 'flymake)
 
-(define-key flymake-mode-map (kbd "C-c .") 'flymake-goto-next-error)
-(define-key flymake-mode-map (kbd "C-c ,") 'flymake-goto-prev-error)
+(define-key flymake-mode-map (kbd "C-x .") 'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "C-x ,") 'flymake-goto-prev-error)
