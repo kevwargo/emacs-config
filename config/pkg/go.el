3b530cf87ff1ab4d919c4c1a1daa2e4efb419821
kevwargo <jarasz.kevwargo@gmail.com>
2024-03-08 20:59:07 +0100

    Implement `go-lsp-find-implementation`

--- config/pkg/go.el
+++ config/pkg/go.el
@@ -30,11 +30,35 @@
                                "pascalcase"
                                "keep"))))))
 
+(defun go-lsp-find-implementation ()
+  "Find the non-mock implementations of the method at point.
+
+If there is exactly one such implementation, jump to it immediately without showing the xref buffer."
+  (interactive)
+  (let* ((lsp-xref-force-references t)
+         (xref-callback xref-show-definitions-function)
+         (xref-show-definitions-function
+          (lambda (fetcher options)
+            (let ((xrefs (--remove
+                          (s-ends-with? "_mock.go"
+                                        (-> it
+                                            xref-item-location
+                                            xref-file-location-file
+                                            file-name-nondirectory))
+                          (funcall fetcher))))
+              (funcall xref-callback
+                       (-const xrefs)
+                       (append `((auto-jump . ,(= (length xrefs) 1)))
+                               options))))))
+    (lsp-find-implementation)))
+
 (defun setup-go-mode ()
-  (add-hook 'before-save-hook 'gofmt-before-save 0 t)
-  (local-set-key (kbd "C-{") 'embrace-selected-lines)
-  (local-set-key (kbd "C-x V") 'go-toggle-test-verbose)
-  (local-set-key (kbd "C-x t") 'go-tag-refresh-json)
-  (local-set-key (kbd "C-x T") 'go-tag-add-json))
+  (add-hook 'before-save-hook 'gofmt-before-save 0 t))
 
 (add-hook 'go-mode-hook 'setup-go-mode)
+
+(define-key go-mode-map (kbd "C-{") 'embrace-selected-lines)
+(define-key go-mode-map (kbd "C-x V") 'go-toggle-test-verbose)
+(define-key go-mode-map (kbd "C-x t") 'go-tag-refresh-json)
+(define-key go-mode-map (kbd "C-x T") 'go-tag-add-json)
+(define-key go-mode-map (kbd "M-/") 'go-lsp-find-implementation)
