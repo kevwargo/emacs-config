68c4bd918fe0d663dbf19420dbf9136efc574d63
kevwargo <jarasz.kevwargo@gmail.com>
2024-08-14 22:31:57 +0200

    Use package-selected-packages when checking for installed packages

--- config/pkg-install.el
+++ config/pkg-install.el
@@ -1,45 +1,5 @@
-(defvar kec:packages nil)
-(setq kec:packages
-      '(
-        company
-        connection
-        default-text-scale
-        dockerfile-mode
-        editorconfig
-        f
-        go-mode
-        go-tag
-        gotest
-        goto-last-change
-        graphql-mode
-        indent-tools
-        json-mode
-        lsp-java
-        lsp-mode
-        magit
-        markdown-mode
-        multi-term
-        neotree
-        paredit
-        powershell
-        prettier
-        project
-        pylint
-        python-black
-        python-isort
-        restclient
-        restclient-jq
-        s
-        string-inflection
-        typescript-mode
-        undo-tree
-        uuidgen
-        yaml-mode
-        yasnippet
-        ))
-
 (defun kec:install-packages ()
-  (dolist (pkg kec:packages)
+  (dolist (pkg package-selected-packages)
     (if (package-installed-p pkg)
         (message "Package %S is already installed" pkg)
       (message "Installing package %S..." pkg)
