a09e595e49e322994a201051e577e941d41d272c
kevwargo <jarasz.kevwargo@gmail.com>
2024-11-07 16:39:18 +0100

    Improve style in keymap-prettify

--- utils/keymaps.el
+++ utils/keymaps.el
@@ -1,16 +1,15 @@
 (defun keymap-prettify (kmap)
-  (if (null (keymapp kmap))
-      nil
-    (list 'keymap
-          (mapcar (lambda (e)
-                    (if (consp e)
-                        (cl-destructuring-bind (key . def) e
-                          (cons (key-description (if (sequencep key)
-                                                     key
-                                                   (vector key)))
-                                (cond
-                                 ((keymapp def) (keymap-prettify def))
-                                 ((symbolp def) def)
-                                 (t 'garbage))))
-                      e))
-                  (cdr-safe kmap)))))
+  (if (keymapp kmap)
+      (list 'keymap
+            (mapcar (lambda (e)
+                      (if (consp e)
+                          (cl-destructuring-bind (key . def) e
+                            (cons (key-description (if (sequencep key)
+                                                       key
+                                                     (vector key)))
+                                  (cond
+                                   ((keymapp def) (keymap-prettify def))
+                                   ((symbolp def) def)
+                                   (t 'garbage))))
+                        e))
+                    (cdr-safe kmap)))))
