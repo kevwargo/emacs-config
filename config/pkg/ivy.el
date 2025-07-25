53a674551b84a9321881b960586dc16eeff50a75
kevwargo <jarasz.kevwargo@gmail.com>
2025-07-25 15:10:31 +0200

    Show whole file path in ivy-xref

--- config/pkg/ivy.el
+++ config/pkg/ivy.el
@@ -2,6 +2,7 @@
 (require 'ivy-xref)
 
 (setq ivy-wrap t)
+(setq ivy-xref-use-file-path t)
 
 (setq xref-show-xrefs-function 'ivy-xref-show-xrefs)
 (setq xref-show-definitions-function 'ivy-xref-show-defs)
