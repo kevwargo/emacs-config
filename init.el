741024d225c829d810dff85f7985882425a6f049
kevwargo <jarasz.kevwargo@gmail.com>
2025-07-26 06:01:06 +0200

    Prevent garbage files in config dir from being autoloaded

--- init.el
+++ init.el
@@ -3,12 +3,11 @@
 
 (defun load-kec-directory (dir)
   (setq dir (expand-file-name dir kec-config-dir))
-  (dolist (item (directory-files dir))
-    (unless (member item '("." ".."))
-      (setq item (expand-file-name item dir))
-      (if (file-directory-p item)
-          (load (expand-file-name (file-name-base item) item) t)
-        (load item)))))
+  (dolist (item (directory-files dir nil "^[a-zA-Z0-9_-]+\\(\\.elc?\\)?$"))
+    (setq item (expand-file-name item dir))
+    (if (file-directory-p item)
+        (load (expand-file-name (file-name-base item) item) t)
+      (load item))))
 
 (load-kec-directory "00-basic")
 (load-kec-directory "01-straight-init")
