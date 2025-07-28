d89d2c26c56a301cd62cf3846b4980ac80ddafe7
kevwargo <jarasz.kevwargo@gmail.com>
2025-07-28 19:21:37 +0200

    small refactor

--- init.el
+++ init.el
@@ -3,8 +3,7 @@
 
 (defun load-kec-directory (dir)
   (setq dir (expand-file-name dir kec-config-dir))
-  (dolist (item (directory-files dir nil "^[a-zA-Z0-9_-]+\\(\\.elc?\\)?$"))
-    (setq item (expand-file-name item dir))
+  (dolist (item (directory-files dir t "^[a-zA-Z0-9_-]+\\(\\.elc?\\)?$"))
     (if (file-directory-p item)
         (load (expand-file-name (file-name-base item) item) t)
       (load item))))
