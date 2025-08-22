a69143b6e725e1e6b506f75658e0cc7f1d3168b7
kevwargo <jarasz.kevwargo@gmail.com>
2025-08-23 01:30:24 +0200

    Bind "C-c TAB" to yas-expand

--- 02-pkg-dependent/yasnippet.el
+++ 02-pkg-dependent/yasnippet.el
@@ -4,7 +4,7 @@
 
 (keymap-unset yas-minor-mode-map "TAB")
 (keymap-unset yas-minor-mode-map "<tab>")
-(keymap-set yas-minor-mode-map "C-c TAB" yas-maybe-expand)
+(keymap-set yas-minor-mode-map "C-c TAB" 'yas-expand)
 (keymap-set yas-minor-mode-map "C-c & l" 'yas-reload-all)
 
 (add-hook 'find-file-hook 'yas-reload-all)
