01f92ef742b5ea4f39dcfdb457e3f9a35adb49e0
kevwargo <jarasz.kevwargo@gmail.com>
2025-10-07 14:37:59 +0200

    Implement `uuid' allowing to regenerate inserted UUIDs

---
+++ 02-pkg-dependent/uuid.el
@@ -0,0 +1,14 @@
+(require 'uuidgen)
+
+(defun uuid ()
+  (interactive)
+  (let ((oldpos (point)))
+    (insert (uuidgen-4))
+    (set-transient-map (let ((m (make-sparse-keymap)))
+                         (keymap-set m "u" (lambda ()
+                                             (interactive)
+                                             (delete-region oldpos (point))
+                                             (insert (uuidgen-4))))
+                         m)
+                       t nil
+                       "Use %k to generate new UUID")))
