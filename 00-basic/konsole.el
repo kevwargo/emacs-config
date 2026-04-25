24ae8e23c0381f1097adb09f6e01a302cb5cb715
kevwargo <jarasz.kevwargo@gmail.com>
2026-04-25 05:15:57 +0300

    Use konsctl for konsole-cd if possible

--- 00-basic/konsole.el
+++ 00-basic/konsole.el
@@ -1,6 +1,8 @@
 (defun konsole-cd (dirname)
   (interactive
    (list (ido-read-directory-name "Konsole-cd: ")))
-  (start-process "konsole-cd" nil "konsole-cd" dirname))
+  (if-let* ((konsctl (executable-find "konsctl")))
+      (call-process konsctl nil nil nil "cd" dirname)
+    (start-process "konsole-cd" nil "konsole-cd" dirname)))
 
 (keymap-global-set "C-x c" 'konsole-cd)
