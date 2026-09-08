e3e752d4569fc660e5aab78429549b5329e91bb6
kevwargo <jarasz.kevwargo@gmail.com>
2026-09-09 01:21:39 +0200

    Fix "C-c C-c" binding in prog-mode

--- 00-basic/prog.el
+++ 00-basic/prog.el
@@ -1,12 +1,12 @@
+;; -*- lexical-binding: t -*-
+
 (defun prog-make-build ()
   (interactive)
   (when-let ((prj-root (locate-dominating-file (buffer-file-name) "Makefile")))
     (message "Running 'make build' in %s" prj-root)
     (message "%s" (with-temp-buffer
                     (let ((default-directory prj-root))
-                      (shell-command "make build" (current-buffer) (current-buffer)))
+                      (call-process "make" nil (list t t) nil "build"))
                     (buffer-string)))))
 
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (keymap-local-set "C-c C-c" 'prog-make-build)))
+(keymap-set prog-mode-map "C-c C-c" 'prog-make-build)
