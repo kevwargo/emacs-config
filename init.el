38dcbd6afa8d814b747ff58e3cafa5a6a56b77c3
kevwargo <jarasz.kevwargo@gmail.com>
2026-09-09 11:41:51 +0200

    Add init load benchmark

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
   (dolist (item (directory-files dir t "^[a-zA-Z0-9_-]+\\(\\.elc?\\)?$"))
     (load item)))
 
-(load-kec-directory "00-basic")
-(load-kec-directory "01-straight-init")
-(load-kec-directory "02-pkg-dependent")
+(let ((ts (current-time)))
+  (load-kec-directory "00-basic")
+  (load-kec-directory "01-straight-init")
+  (load-kec-directory "02-pkg-dependent")
+  (message "Total init load time: %fs" (float-time (time-since ts))))
